7d261baca65973b9115821780c4c797d247d0bfc
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-06-02 19:04:12 +0200

    Cleaned up the code by creating a new function. Also created a new feature which is to determine the winner of the game

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 let deckId;
 
+const newDeckBtn = document.getElementById("new-deck");
 const drawCardsBtn = document.getElementById("draw-cards-btn");
 
-function handleClick() {
+function renderCardSlots(data) {
+    const cardSlots = document.getElementById("cards-container").children;
+    for (let i = 0; i < cardSlots.length; i++) {
+        cardSlots[i].innerHTML = "";
+        cardSlots[i].innerHTML = `<img src="${data.cards[i].image}" alt="The code of the card is: ${data.cards[i].code}">`;
+    }
+}
+
+function getNewDeck() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
@@ -18,13 +27,47 @@ function drawCards() {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-            const cardSlots = document.getElementById("cards-container").children;
-            for (let i = 0; i < cardSlots.length; i++) {
-                cardSlots[i].innerHTML = "";
-                cardSlots[i].innerHTML = `<img src="${data.cards[i].image}" alt="The code of the card is: ${data.cards[i].code}">`;
-            }
+            renderCardSlots(data);
+            let winningCard = determineWinningCard(data.cards[0].value, data.cards[1].value);
+            console.log(winningCard);
         });
 }
 
-document.getElementById("new-deck").addEventListener("click", handleClick);
+newDeckBtn.addEventListener("click", getNewDeck);
 drawCardsBtn.addEventListener("click", drawCards);
+
+function determineWinningCard(card1, card2) {
+    const cardValues = {
+        1: 1,
+        2: 2,
+        3: 3,
+        4: 4,
+        5: 5,
+        6: 6,
+        7: 7,
+        8: 8,
+        9: 9,
+        10: 10,
+        JACK: 11,
+        QUEEN: 12,
+        KING: 13,
+        ACE: 14
+    };
+    
+    const card1Value = cardValues[card1];
+    const card2Value = cardValues[card2];
+
+    console.log(`Card 1: ${card1Value}`);
+    console.log(`Card 2: ${card2Value}`);
+
+    if (card1Value < card2Value) {
+        console.log("Card 2 won");
+        return card2Value;
+    } else if (card1Value > card2) {
+        console.log("Card 1 won");
+        return card1Value;
+    } else {
+        console.log("It's a tie");
+        return null;
+    }
+}
